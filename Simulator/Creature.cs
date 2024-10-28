52af813c9c19b3cf2d9561bf0e9c83535d92d7d5
Wyzgaa <jakubwyzgaa@gmail.com>
2024-10-29 00:18:01 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    public class Creature
+    {
+        private string? name;
+        private int level;
+        public string? Name
+        {
+            get
+            {
+                return name;
+            }
+            set { name = value; }
+        }
+        public int Level
+        {
+            get => level;
+            init
+            {
+                if (value < 0)
+                {
+                    level = 1;
+                }
+                else
+                {
+                    level = value;
+                }
+            }
+        }
+        public Creature() { }
+
+        public Creature(string name, int level)
+        {
+            Name=name;
+            Level = level;
+        }
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+        }
+        public string Info
+        {
+            get { return $"{Name} [{Level}]"; }
+        }
+
+    }
+}
+
