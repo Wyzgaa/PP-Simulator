1612fc8ea4cefcb720ff92c2ef1e611d7019dd53
Wyzgaa <jakubwyzgaa@gmail.com>
2024-10-29 22:04:46 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -10,7 +10,35 @@ namespace Simulator
 {
     public class Animals
     {
-        public required string Description { get; init; }
+        private string description="Unknown";
+        public required string Description
+        {
+            get
+            {
+                return description;
+            }
+            init
+            {
+                if (value == null) { return; }
+                string temp_name = value.Trim();
+                if (temp_name.Length > 0)
+                {
+                    if (temp_name.Length > 15)
+                    {
+                        temp_name = temp_name.Substring(0, 15);
+                        temp_name = temp_name.Trim();
+                    }
+                    temp_name = char.ToUpper(temp_name[0]) + temp_name.Substring(1);
+                    if (temp_name.Length < 3)
+                    {
+                        temp_name = temp_name + string.Concat(Enumerable.Repeat("#", 3 - temp_name.Length));
+                    }
+                }
+                if (temp_name == "")
+                    temp_name = "Unknown";
+                description = temp_name;
+            }
+        }
         public uint Size { get; set; } = 3;
         public string Info
         {
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,34 +9,66 @@ namespace Simulator
 {
     public class Creature
     {
-        private string? name;
-        private int level;
+        private string? name = "Unknown";
+        private int level=1;
         public string? Name
         {
             get
             {
                 return name;
             }
-            set { name = value; }
+            init
+            {
+                if (value == null) { return; }
+                string temp_name = value.Trim();
+                if(temp_name.Length>0)
+                {
+                    if (temp_name.Length > 25)
+                    {
+                        temp_name = temp_name.Substring(0, 25);
+                        temp_name = temp_name.Trim();
+                    }
+                    temp_name = char.ToUpper(temp_name[0]) + temp_name.Substring(1);
+                    if (temp_name.Length < 3)
+                    {
+                        temp_name = temp_name + string.Concat(Enumerable.Repeat("#", 3 - temp_name.Length));
+                    }
+                }
+                if (temp_name == "")
+                    temp_name = "Unknown";
+                name = temp_name;
+            }
         }
         public int Level
         {
             get => level;
             init
             {
-                if (value < 0)
+                if (value <= 0)
                 {
                     level = 1;
+                }
+                else if (value > 10)
+                {
+                    level = 10;
                 }
                 else
                 {
                     level = value;
                 }
+                
+            }
+        }
+        public void Upgrade()
+        {
+            if(level <10)
+            {
+                level++;
             }
         }
         public Creature() { }
 
-        public Creature(string name, int level)
+        public Creature(string name, int level=1)
         {
             Name=name;
             Level = level;
