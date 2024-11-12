bb58000ed444287cee9d0799d24b39460a31c5e6
Wyzgaa <jakubwyzgaa@gmail.com>
2024-11-12 23:20:51 +0100

    Prostokąty

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,12 +1,41 @@
-﻿namespace Simulator;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Simulator;
 
 internal class Program
 {
+    static void Lab5a() 
+    {
+        var a = new Rectangle(5, 7, 1, 2);
+        Console.WriteLine(a.ToString());
+        var b = new Rectangle(1, 7, 1, 2);
+        var c = new Rectangle(5, 7, 1, 7);
+        var d = new Rectangle(1, 2, 5, 4);
+        Console.WriteLine(d.ToString());
+        var p1= new Point(1, 1);
+        var p2 = new Point(1, 2);
+        var p3 = new Point(1, 1);
+        var p4 = new Point(5, 5);
+        var e = new Rectangle(p1, p2);
+        var f = new Rectangle(p3, p4);
+        Console.WriteLine(f.ToString());
+        var g = new Point(2, 5);
+        var h = new Point(1,8);
+        var i = new Point(1,1);
+        var j = new Point(-1,4);
+        Console.WriteLine(f.Contains(g));
+        Console.WriteLine(f.Contains(h));
+        Console.WriteLine(f.Contains(i));
+        Console.WriteLine(f.Contains(j));
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
-        Point p = new(10, 25);
-        Console.WriteLine(p.Next(Direction.Right));          // (11, 25)
-        Console.WriteLine(p.NextDiagonal(Direction.Right));  // (11, 24)
+        //Point p = new(10, 25);
+        //Console.WriteLine(p.Next(Direction.Right));          // (11, 25)
+        //Console.WriteLine(p.NextDiagonal(Direction.Right));  // (11, 24)
+
+
+        Lab5a();
     }
 }
--- Simulator/Rectangle.cs
+++ Simulator/Rectangle.cs
@@ -12,20 +12,48 @@ public class Rectangle
     public readonly int X1, Y1, X2, Y2;
     public Rectangle(int x1, int y1, int x2, int y2)
     {
-       try
+        try
         {
             if (x1 == x2 || y1 == y2)
-                throw new ArgumentException("Punkty współliniowe");
-            x1 = X1;
-            x2 = X2;
-            y1 = Y1;
-            y2 = Y2;
+                throw new ArgumentException();
+            if (x1 > x2)
+            {
+                X1 = x2;
+                X2 = x1;
+            }
+            else
+            {
+                X1 = x1;
+                X2 = x2;
+            }
+            if (y1 > y2)
+            {
+                Y1 = y2;
+                Y2 = y1;
+            }
+            else
+            {
+                Y1 = y1;
+                Y2 = y2;
+            }
         }
-        catch
+        catch (ArgumentException)
         {
-
+            Console.WriteLine("nie chcemy 'chudych' prostokątów");
         }
-
-        
+            
     }
+    public Rectangle(Point p1, Point p2) : this(p1.X, p1.Y, p2.X, p2.Y) { }
+    public bool Contains(Point point) 
+    {
+        if(point.X>=X1 && point.X<=X2 && point.Y>=Y1 && point.Y<=Y2)
+            return true;
+        else 
+            return false;
+    }
+    public override string ToString() 
+    {
+        return ($"({X1},{Y1}):({X2},{Y2})");
+    }
+
 }
