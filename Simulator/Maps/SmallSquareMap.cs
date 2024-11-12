07584ece95e3415fc1d9c35f652f12cc0caeb0b6
Wyzgaa <jakubwyzgaa@gmail.com>
2024-11-13 00:04:42 +0100

    Lab-5

---
+++ Simulator/Maps/SmallSquareMap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator.Maps;
+
+public class SmallSquareMap : Map
+{
+    public int Size;
+
+    public SmallSquareMap(int size)
+    {
+        if (size < 5 || size > 20)
+            throw new ArgumentOutOfRangeException("Niewłaściwy rozmiar mapy");
+        Size = size;
+    }
+    public override bool Exist(Point p)
+    {
+        if(p.X>=0 && p.Y>=0 && p.X<=Size-1 && p.Y <= Size - 1)
+            return true;
+        return false;
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        if(Exist(p.Next(d)))
+            return p.Next(d);
+        return p;
+    }
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        if(Exist(p.NextDiagonal(d)))
+            return p.NextDiagonal(d);
+        return p;
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,10 +1,11 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using Simulator.Maps;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Simulator;
 
 internal class Program
 {
-    static void Lab5a() 
+    static void Lab5a()
     {
         var a = new Rectangle(5, 7, 1, 2);
         Console.WriteLine(a.ToString());
@@ -12,7 +13,7 @@ internal class Program
         var c = new Rectangle(5, 7, 1, 7);
         var d = new Rectangle(1, 2, 5, 4);
         Console.WriteLine(d.ToString());
-        var p1= new Point(1, 1);
+        var p1 = new Point(1, 1);
         var p2 = new Point(1, 2);
         var p3 = new Point(1, 1);
         var p4 = new Point(5, 5);
@@ -20,14 +21,52 @@ internal class Program
         var f = new Rectangle(p3, p4);
         Console.WriteLine(f.ToString());
         var g = new Point(2, 5);
-        var h = new Point(1,8);
-        var i = new Point(1,1);
-        var j = new Point(-1,4);
+        var h = new Point(1, 8);
+        var i = new Point(1, 1);
+        var j = new Point(-1, 4);
         Console.WriteLine(f.Contains(g));
         Console.WriteLine(f.Contains(h));
         Console.WriteLine(f.Contains(i));
         Console.WriteLine(f.Contains(j));
     }
+    static void Lab5b()
+    {
+        var a = new SmallSquareMap(10);
+
+        try 
+        { 
+            var b = new SmallSquareMap(21); 
+        }
+        catch (Exception exept)
+        { 
+            Console.WriteLine($"{exept.Message}"); 
+        }
+
+        try
+        {
+            var c = new SmallSquareMap(4);
+        }
+        catch (Exception exe)
+        {
+            Console.WriteLine($"{exe.Message}");
+        }
+
+        Console.WriteLine(a.Exist(new Point(4, 4))); //T
+        Console.WriteLine(a.Exist(new Point(10, 6))); //F
+        Console.WriteLine(a.Exist(new Point(4, 10))); //F
+        Console.WriteLine(a.Exist(new Point(1, 9))); //T
+        Console.WriteLine(a.Exist(new Point(0, 8))); //T
+
+        Console.WriteLine(a.Next(new Point(5, 5), Direction.Up));
+        Console.WriteLine(a.Next(new Point(5, 5), Direction.Down));
+        Console.WriteLine(a.Next(new Point(5, 5), Direction.Left)); 
+        Console.WriteLine(a.Next(new Point(5, 5), Direction.Right));
+        Console.WriteLine(a.Next(new Point(9, 9), Direction.Right));
+        
+        Console.WriteLine(a.NextDiagonal(new Point(5, 5), Direction.Left));
+        Console.WriteLine(a.NextDiagonal(new Point(5, 5), Direction.Right));
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
@@ -35,7 +74,7 @@ internal class Program
         //Console.WriteLine(p.Next(Direction.Right));          // (11, 25)
         //Console.WriteLine(p.NextDiagonal(Direction.Right));  // (11, 24)
 
-
         Lab5a();
+        Lab5b();
     }
 }
