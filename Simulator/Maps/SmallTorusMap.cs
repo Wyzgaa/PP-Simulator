132a91da5124e85f00683168f85071db67c28b77
Wyzgaa <jakubwyzgaa@gmail.com>
2024-11-13 16:19:25 +0100

    SmallTorusMap added

---
+++ Simulator/Maps/SmallTorusMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator.Maps
+{
+    public class SmallTorusMap : Map
+    {
+        public override bool Exist(Point p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Point Next(Point p, Direction d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Point NextDiagonal(Point p, Direction d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
