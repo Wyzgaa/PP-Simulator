a35d53e6a3965ab98ea2e93134d39ea6119fe896
Wyzgaa <jakubwyzgaa@gmail.com>
2024-11-19 19:10:42 +0100

    TDD

--- Simulator/Maps/SmallTorusMap.cs
+++ Simulator/Maps/SmallTorusMap.cs
@@ -8,19 +8,54 @@ namespace Simulator.Maps
 {
     public class SmallTorusMap : Map
     {
+        private int size;
+        public int Size 
+        {
+            get { return size; }
+        }
         public override bool Exist(Point p)
         {
-            throw new NotImplementedException();
+            if (p.X >= 0 && p.Y >= 0 && p.X <= Size - 1 && p.Y <= Size - 1)
+                return true;
+            return false;
         }
 
         public override Point Next(Point p, Direction d)
         {
-            throw new NotImplementedException();
+            int x=p.X, y=p.Y;
+            var nextpoint = p.Next(d);
+            if (Exist(nextpoint))
+                return nextpoint;
+            if (nextpoint.X >= size || nextpoint.X < 0)
+                x = (nextpoint.X+size)%size;
+            if (nextpoint.Y >= size || nextpoint.Y < 0)
+                y = (nextpoint.Y + size) % size;
+            Point result = new Point(x, y);
+            return result;
         }
 
         public override Point NextDiagonal(Point p, Direction d)
         {
-            throw new NotImplementedException();
+            int x = p.X, y = p.Y;
+            var nextpoint = p.NextDiagonal(d);
+
+            if (Exist(nextpoint))
+                return nextpoint;
+            if (nextpoint.X >= size || nextpoint.X < 0)
+                x = (nextpoint.X + size) % size;
+            else x = nextpoint.X;
+            if (nextpoint.Y >= size || nextpoint.Y < 0)
+                y = (nextpoint.Y + size) % size;
+            else y = nextpoint.Y;
+            Point result = new Point(x, y);
+            return result;
+        }
+
+        public SmallTorusMap(int MapSize) 
+        {
+            if(MapSize<5 || MapSize >20)
+                throw new ArgumentOutOfRangeException("Rozmiar mapy musi zawierać się w przedziale [5,20]");
+            size= MapSize;
         }
     }
 }
